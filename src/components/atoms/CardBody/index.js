45c982813ca2ee8b6d7e98aefd7acf39a71e014b
Semih Kaya <kayasemih95@gmail.com>
2022-10-29 23:55:46 +0300

    separate card hover from CardBody Comp to new CardHover Comp

--- src/components/atoms/CardBody/index.js
+++ src/components/atoms/CardBody/index.js
@@ -1,29 +1,25 @@
 import PropTypes from "prop-types";
-import { useToggle } from "../../../hooks/useToggle";
-import { ellipsisIcon } from "../../../icons/ellipsisIcon";
-import { Icon } from "../Icon";
 import "./styles.css";
 
-
-export const CardBody = ({ children }) => {
-
-  const [isHover, toggleIsHover] = useToggle(false);
+export const CardBody = ({ 
+  children, 
+  onMouseEnter = () => {}, 
+  onMouseLeave = () => {}
+}) => {
 
   return (
-    <div className="card-container" onMouseEnter={toggleIsHover} onMouseLeave={toggleIsHover}>
-      {isHover && (
-        <>
-          <div className="card-container-overlay"></div>
-          <div className="card-edit-icon">
-            <Icon svg={ellipsisIcon} />
-          </div>
-        </>
-      )}
+    <div 
+      className="card-container" 
+      onMouseEnter={onMouseEnter} 
+      onMouseLeave={onMouseLeave}
+    >
       { children }
     </div>
   )
 }
 
 CardBody.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func
 }
--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useToggle } from '../../../hooks/useToggle';
 import { CardBody } from '../../atoms/CardBody';
+import { CardHover } from '../../atoms/CardHover';
 import { CardTitle } from '../../atoms/CardTitle';
 import { Tag } from '../../atoms/Tag';
 import "./styles.css";
 
 export const Card = () => {
+
+  const [isHover, toggleIsHover] = useToggle(false);
+
   return (
-    <CardBody>
+    <CardBody onMouseEnter={toggleIsHover} onMouseLeave={toggleIsHover}>
+      <CardHover isHover={isHover} />
       <CardTitle title={"Wellcome to our trello app. Begin your jorney. First, create your first list."} />
       <div className='card-tag-wrapper'>
         <Tag tagName={"new-user"} colorIndex={0} />
